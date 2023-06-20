138d5b47a7ee4c4ea2f2623f5afe514bd0857830
Parham-Mehrabi <parham.mehrabi.webdev@gmail.com>
2023-06-20 18:50:06 +0330

    fetch body of resume from backend

--- frontend/src/components/resume.jsx
+++ frontend/src/components/resume.jsx
@@ -1,8 +1,23 @@
+import {useEffect, useState} from "react";
 
-export default function Resume (){
+
+export default function Resume() {
+    useEffect(() => getResume, [])
+    const [resume, setResume] = useState()
     return (
         <div className='text-white'>
-            <h2 className='text-center'>My Resume</h2>
+            {resume && resume['body'] ? (
+                <h2 className='text-center'>{resume['body']}</h2>
+            ) : null
+                //TODO: create a loading something for here
+            }
         </div>
     )
+
+    async function getResume() {
+        const response = await fetch('http://127.0.0.1:8000/back/resume')
+        // TODO: use base_url instead of hand coded url
+        const data = await response.json()
+        setResume(data)
+    }
 }
