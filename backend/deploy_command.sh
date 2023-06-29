7a899ece4c8d11ebab408f018b0d857fd9ac0d8b
Parham-Mehrabi <parham.mehrabi.webdev@gmail.com>
2023-06-29 21:47:06 +0330

    fix the gunicorn port and nginx upstream name

--- backend/deploy_command.sh
+++ backend/deploy_command.sh
@@ -1,4 +1,4 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn core.wsgi --bind 0.0.0.0:8002 --access-logfile -  --error-logfile -
+gunicorn core.wsgi --bind 0.0.0.0:8003 --access-logfile -  --error-logfile -
