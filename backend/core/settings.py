7a620ce460094723b2bb83784f3840cd09dec952
Parham-Mehrabi <parham.mehrabi.webdev@gmail.com>
2023-06-29 18:06:56 +0330

    change db to psql

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -64,10 +64,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'HOST': 'psql',
+        'PORT': '5432',
+        'USER': 'parham',
+        'PASSWORD': os.getenv('PARHAM_PASSWORD'),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
